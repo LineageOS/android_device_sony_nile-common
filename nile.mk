81be61d18c265bed450c820f399979f24251c842
Edwin Wong <edwinwong@google.com>
2018-01-31 10:29:05 -0800

    nile-common: Add SELinux policy for clearkey HIDL service.
    Convert clearkey plugin to HIDL to support drm HAL v1.1.
    Add SELinux policy for android.hardware.drm@1.1-service.clearkey.
    
    Test: CTS test
      ANDROID_BUILD_TOP= ./android-ccts-tradefed run cts-dev
        --module CtsMediaTestCases
        -t android.media.cts.ClearKeySystemTest#testClearKeyPlaybackCenc
    
    bug: 69635855
    Change-Id: I61e9c272c2a2788fd07d5c12921d28c785661b77

--- nile.mk
+++ nile.mk
@@ -199,7 +199,8 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl:32 \
-    android.hardware.drm@1.0-service
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.1-service.clearkey
 
 # Fingerprint
 PRODUCT_PACKAGES += \
