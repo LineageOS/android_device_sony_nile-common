f3fe372291f377b1e9197385b3b721e794a9aec7
Udaya Bhaskara Reddy Mallavarapu <udaym@codeaurora.org>
2018-02-02 13:28:27 +0530

    nile-common: Enable 64 bit DRM plugins compilation
    Enable 64 bit compilation for DRM plugins.
    
    This will generate DRM plugins, HALs & oemcrypto_test in 64 bit.
    
    Change-Id: I451db39edea3446d9816fcaba77caa8d3b097afb

--- nile.mk
+++ nile.mk
@@ -206,7 +206,7 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl:32 \
+    android.hardware.drm@1.0-impl:64 \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.3-service.clearkey
 
