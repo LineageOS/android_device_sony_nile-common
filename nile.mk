5ea23fc7881b70fb31c624a4a8623a24836a66a1
Kevin Rocard <krocard@google.com>
2018-03-02 19:41:31 -0800

    nile-common: Migrate to audio V4
    Test: regression test on the board
    Bug: 38184704
    Change-Id: Ife559125850277865451f92ea862e7ec47fc9da7
    Merged-In: Ife559125850277865451f92ea862e7ec47fc9da7
    Cherry-picked from master
    Signed-off-by: Kevin Rocard <krocard@google.com>

--- nile.mk
+++ nile.mk
@@ -98,9 +98,9 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl:32 \
+    android.hardware.audio@4.0-impl:32 \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@2.0-impl:32 \
+    android.hardware.audio.effect@4.0-impl:32 \
     android.hardware.soundtrigger@2.0-impl:32 \
     audio.a2dp.default \
     audio.primary.sdm660 \
