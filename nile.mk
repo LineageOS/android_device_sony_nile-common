dc0324fcadb4c86e5c15a454f1c3702b1776e15d
LuK1337 <priv.luk@gmail.com>
2018-03-28 14:58:11 +0200

    nile-common: Import light HAL

--- nile.mk
+++ nile.mk
@@ -265,8 +265,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service
+    android.hardware.light@2.0-service.nile
 
 # Media
 PRODUCT_COPY_FILES += \
