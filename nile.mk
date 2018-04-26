158f5e371d7c8583714dcb25a15b0bc33a6b844b
LuK1337 <priv.luk@gmail.com>
2018-04-27 01:44:56 +0200

    nile-common: Build radio packages

--- nile.mk
+++ nile.mk
@@ -349,6 +349,9 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
+    android.hardware.radio@1.0 \
+    android.hardware.radio@1.1 \
+    android.hardware.radio.deprecated@1.0 \
     libprotobuf-cpp-full \
     librmnetctl \
     libxml2
