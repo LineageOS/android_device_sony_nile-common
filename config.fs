740dc679273ae6eedaa5bf4839bfc8bd435d3e8b
LuK1337 <priv.luk@gmail.com>
2018-06-03 12:03:25 +0200

    nile-common: Remove duplicated config.fs rule

--- config.fs
+++ config.fs
@@ -120,9 +120,3 @@ mode: 0755
 user: AID_WIFI
 group: AID_WIFI
 caps: NET_ADMIN NET_RAW SYS_MODULE
-
-[vendor/bin/hw/android.hardware.wifi@1.0-service]
-mode: 0755
-user: AID_WIFI
-group: AID_WIFI
-caps: NET_ADMIN NET_RAW SYS_MODULE
