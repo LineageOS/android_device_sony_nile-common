219cedd942dc9efafdc5d6a46168ff434f92c366
LuK1337 <priv.luk@gmail.com>
2018-07-01 14:36:32 +0200

    nile-common: Remove init.qcom.crashdata.sh
    Change-Id: Ife137c007eec42e61d2766d1446c77907f5bdd05

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -19,14 +19,6 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qcom.crashdata.sh
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.qcom.crashdata.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.devstart.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
