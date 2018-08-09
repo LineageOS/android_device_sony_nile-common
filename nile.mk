cad61807e0d2993671f8e4970ba24cd71860aff4
LuK1337 <priv.luk@gmail.com>
2018-08-09 16:43:15 +0200

    nile-common: Don't build AOSP keymaster
    * We are using QTI one
    
    Change-Id: I110b095d29438f7621fb2e333db8870f5796c4d6

--- nile.mk
+++ nile.mk
@@ -258,11 +258,6 @@ PRODUCT_PACKAGES += \
     ethertypes \
     libebtc
 
-# Keymaster
-PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl \
-    android.hardware.keymaster@3.0-service
-
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.nile
