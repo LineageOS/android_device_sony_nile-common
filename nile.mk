384fcd212b3bdbabe62c65e66542eea5169e7d63
LuK1337 <priv.luk@gmail.com>
2018-08-11 16:27:29 +0200

    nile-common: Switch to new NXP NFC
    Change-Id: Idbf8506c5e7c77b56d131bcd857c29a643f46685

--- nile.mk
+++ nile.mk
@@ -285,17 +285,11 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.0-impl \
+    android.hardware.nfc@1.1-service \
     com.android.nfc_extras \
-    com.nxp.nfc.nq \
-    com.nxp.nfc.nq.xml \
-    libnqnfc-nci \
-    libnqp61-jcop-kit \
-    nfc_nci.nqx.default \
-    NQNfcNci \
-    Tag \
-    vendor.nxp.hardware.nfc@1.0-impl \
-    vendor.nxp.hardware.nfc@1.0-service
+    NfcNci \
+    SecureElement \
+    Tag
 
 # OMX
 PRODUCT_PACKAGES += \
