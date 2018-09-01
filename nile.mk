a4f78ecfaf5f2d0af3cb99d72d2d046edd989654
LuK1337 <priv.luk@gmail.com>
2018-09-01 18:55:18 +0200

    nile-common: Remove unexisting camera packages
    * We aren't building camera HAL from source.
    
    Change-Id: I35813957c4777ca92b3f2001e3107ff8378a54d8

--- nile.mk
+++ nile.mk
@@ -156,8 +156,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    camera.sdm660 \
-    libmm-qcamera \
     Snap \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
