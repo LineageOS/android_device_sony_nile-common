a1b7bdb16fac051bcb92d84d817da31503a1d430
Ethan Chen <intervigil@gmail.com>
2018-11-30 23:28:49 -0800

    nile-common: Add AOSP RCS packages
    Change-Id: Ic16d3b92820d246db73ad70ccaa0f4bc7f50e515

--- nile.mk
+++ nile.mk
@@ -336,6 +336,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libstdc++.vendor
 
+# RCS
+PRODUCT_PACKAGES += \
+    com.android.ims.rcsmanager \
+    PresencePolling \
+    RcsService
+
 # RenderScript
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
