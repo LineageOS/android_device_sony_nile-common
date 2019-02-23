1d509c236e7d6ff83efae53e3ef6dfdaedc13e98
LuK1337 <priv.luk@gmail.com>
2019-02-23 02:00:41 +0100

    nile-common: Update blobs from 50.2.A.0.400
    * Build vendor libhidl
    * Update gps configs
    * Update SELinux policies
    * Update vendor props
    * Update service definitions
    * Update manifest.xml
    * Update symlinks
    * Create necessary /data/vendor folders while removing /data/misc ones
    
    Change-Id: Id84256f13d068da4a56abda99fb327f00052edb6

--- rootdir/etc/init.charge.mode.sh
+++ rootdir/etc/init.charge.mode.sh
@@ -2,6 +2,6 @@
 
 # For Power Off Charging mode only
 cei_mainboard_id=`cat /proc/cei_mainboard_id`
-setprop ro.cei_mainboard_id $cei_mainboard_id
+setprop ro.vendor.cei_mainboard_id $cei_mainboard_id
 
 
