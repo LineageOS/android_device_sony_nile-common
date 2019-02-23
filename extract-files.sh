352e1f8481d604fbff01ecc03b15434de2e8aee5
LuK1337 <priv.luk@gmail.com>
2019-02-23 02:00:41 +0100

    nile-common: Update blobs from 50.2.A.0.342
    * Build vendor libhidl
    * Update gps configs
    * Update SELinux policies
    * Update vendor props
    * Update service definitions
    * Create necessary /data/vendor folders while removing /data/misc ones
    
    Change-Id: Id84256f13d068da4a56abda99fb327f00052edb6

--- extract-files.sh
+++ extract-files.sh
@@ -65,6 +65,4 @@ fi
 
 COMMON_BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE_COMMON"/proprietary
 
-patchelf --replace-needed android.hardware.gnss@1.0.so android.hardware.gnss@1.0-v27.so $COMMON_BLOB_ROOT/vendor/lib64/vendor.qti.gnss@1.0_vendor.so
-
 "$MY_DIR"/setup-makefiles.sh
