d40c0d57f12d7f2426c567ad62d7b8626e774606
LuK1337 <priv.luk@gmail.com>
2019-02-23 02:00:41 +0100

    nile-common: Update blobs from 50.2.A.0.400
    * Build vendor libhidl
    * Update gps configs
    * Update SELinux policies
    * Update vendor props
    * Update service definitions
    * Update manifest.xml
    * Update symlinks
    * Create necessary /data/vendor folders while removing /data/misc ones
    
    Change-Id: Id84256f13d068da4a56abda99fb327f00052edb6

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -193,7 +193,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 # RIL
 TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 TARGET_RIL_VARIANT := caf
-TARGET_USES_OLD_MNC_FORMAT := true
+ENABLE_VENDOR_RIL_SERVICE := true
 
 # Root
 BOARD_ROOT_EXTRA_FOLDERS := idd rca
@@ -204,7 +204,7 @@ BOARD_ROOT_EXTRA_SYMLINKS := \
     /vendor/firmware_mnt:/firmware
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2018-12-01
+VENDOR_SECURITY_PATCH := 2019-08-01
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
