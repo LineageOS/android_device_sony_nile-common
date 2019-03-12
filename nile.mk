b7f0ad24b41384974cfd828ee496b29d3f39c077
LuK1337 <priv.luk@gmail.com>
2019-03-12 19:43:58 +0100

    nile-common: Sync and build SimDetect
    Change-Id: Ia9a47be3627a7ebd63f241feee0ed3479241967d

--- nile.mk
+++ nile.mk
@@ -346,6 +346,10 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl:64 \
     android.hardware.sensors@1.0-service
 
+# SimDetect
+PRODUCT_PACKAGES += \
+    SimDetect
+
 # Telephony
 PRODUCT_PACKAGES += \
     telephony-ext
