f8987cfced715f0fe5bc05def3472979d0e8844a
LuK1337 <priv.luk@gmail.com>
2019-04-03 13:33:18 +0200

    nile-common: Regenerate config.fs
    Change-Id: Id80d61a8a40f2d20e399db99338401037e5a483d

--- config.fs
+++ config.fs
@@ -59,13 +59,7 @@ caps: 0
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
-caps: NET_BIND_SERVICE BLOCK_SUSPEND NET_ADMIN
-
-[vendor/bin/cnss-daemon]
-mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: NET_BIND_SERVICE NET_ADMIN
+caps: NET_BIND_SERVICE NET_ADMIN BLOCK_SUSPEND
 
 [vendor/bin/imsdatadaemon]
 mode: 0755
@@ -73,17 +67,23 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
+[vendor/bin/imsrcsd]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: WAKE_ALARM
+
 [vendor/bin/ims_rtp_daemon]
 mode: 0755
 user: AID_SYSTEM
 group: AID_RADIO
 caps: NET_BIND_SERVICE
 
-[vendor/bin/imsrcsd]
+[vendor/bin/pd-mapper]
 mode: 0755
 user: AID_SYSTEM
-group: AID_RADIO
-caps: WAKE_ALARM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
 
 [vendor/bin/pm-service]
 mode: 0755
@@ -91,15 +91,9 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
-[vendor/bin/pm-proxy]
-mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: NET_BIND_SERVICE
-
 [vendor/bin/slim_daemon]
 mode: 0755
-user:  AID_GPS
+user: AID_GPS
 group: AID_GPS
 caps: NET_BIND_SERVICE
 
@@ -107,16 +101,16 @@ caps: NET_BIND_SERVICE
 mode: 0755
 user: AID_BLUETOOTH
 group: AID_BLUETOOTH
-caps: BLOCK_SUSPEND SYS_NICE
+caps: BLOCK_SUSPEND
 
 [vendor/bin/xtwifi-client]
 mode: 0755
-user:  AID_GPS
+user: AID_GPS
 group: AID_GPS
 caps: NET_BIND_SERVICE BLOCK_SUSPEND
 
-[vendor/bin/hw/android.hardware.wifi@1.0-service]
+[vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti]
 mode: 0755
-user: AID_WIFI
-group: AID_WIFI
-caps: NET_ADMIN NET_RAW SYS_MODULE
+user: AID_BLUETOOTH
+group: AID_BLUETOOTH
+caps: NET_ADMIN BLOCK_SUSPEND
