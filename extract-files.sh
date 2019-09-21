3eb5930c5d3df7a2e9a85d34d202a72168405ad1
Kshitij Gupta <kshitijgm@gmail.com>
2019-09-21 11:35:30 +0800

    nile-common: Fix blobs that referenced set_sched_policy
    * This is now moved to libcutils
    
    Change-Id: I9e204235efc62059f07127a2df455f911dc936b7

--- extract-files.sh
+++ extract-files.sh
@@ -30,6 +30,17 @@ if [ ! -f "${HELPER}" ]; then
 fi
 source "${HELPER}"
 
+function blob_fixup() {
+    case "${1}" in
+    vendor/lib/libgps.utils.so)
+        patchelf --replace-needed "libcutils.so" "libprocessgroup.so" "${2}"
+        ;;
+    vendor/lib64/libgps.utils.so)
+        patchelf --replace-needed "libcutils.so" "libprocessgroup.so" "${2}"
+        ;;
+    esac
+}
+
 # Default to sanitizing the vendor folder before extraction
 CLEAN_VENDOR=true
 
