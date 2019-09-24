75100773d21aacf5e113be13036c10752ae661c2
Tapas Dey <tdey@codeaurora.org>
2019-09-24 13:08:20 +0530

    nile-common: Update Nfc Interfaces version
    Updated the INfc and INqNfc version to
    enable new upgraded interfaces solution.
    
    Change-Id: Iedc413cbbfa515c3e2dbeb052d6eb472734f9a04

--- nile.mk
+++ nile.mk
@@ -278,7 +278,7 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.1-service \
+    android.hardware.nfc@1.2-service \
     com.android.nfc_extras \
     NfcNci \
     SecureElement \
