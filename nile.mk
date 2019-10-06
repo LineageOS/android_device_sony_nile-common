0cf0cd45693b5765317782401bf152eda872a7ba
LuK1337 <priv.luk@gmail.com>
2019-10-06 22:52:17 +0200

    nile-common: Build thermal HAL
    Change-Id: I6d066aa97adef17fae376dfec392ac97bb89a963

--- nile.mk
+++ nile.mk
@@ -365,7 +365,8 @@ PRODUCT_PACKAGES += \
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service
+    android.hardware.thermal@1.0-service \
+    thermal.sdm660
 
 # Update engine
 PRODUCT_PACKAGES += \
