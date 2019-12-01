34fbd356927dc9e03c7cb6b0fa40907addc192be
LuK1337 <priv.luk@gmail.com>
2019-12-01 22:52:11 +0100

    nile-common: Workaround SELinux init-qcom-cei-sh setprop neverallow
    Change-Id: I5cdf3c95c635d829645f8d365259bc4eef57e3df

--- rootdir/etc/init.qcom.cei.sh
+++ rootdir/etc/init.qcom.cei.sh
@@ -1,8 +1,8 @@
 #!/vendor/bin/sh
 
-setprop persist.radio.multisim.config "$(cat /proc/cei_simslot_id | xargs)"
 setprop ro.vendor.cei_mainboard_id "$(cat /proc/cei_mainboard_id | xargs)"
 setprop ro.vendor.cei_phase_id "$(cat /proc/cei_phase_id | xargs)"
 setprop ro.vendor.cei_project_id "$(cat /proc/cei_project_id | xargs)"
 setprop ro.vendor.cei_sku_id "$(cat /proc/cei_sku_id | xargs)"
+setprop ro.vendor.radio.multisim.config "$(cat /proc/cei_simslot_id | xargs)"
 setprop vendor.hw.fingerprint "$(cat /proc/cei_fp_id | xargs)"
