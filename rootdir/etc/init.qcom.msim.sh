648d811cdd95c8384ab6cbb290276d0edd1ac9d0
LuK1337 <priv.luk@gmail.com>
2019-12-08 23:08:57 +0100

    nile-common: Move to non-CEI friendly msim detection
    Change-Id: I7abbbdfec3e57877205b67b638981a2a0eddc20f

---
+++ rootdir/etc/init.qcom.msim.sh
@@ -0,0 +1,19 @@
+#!/vendor/bin/sh
+MSIM_DEVICES=(
+    H4113 H4133 # XA2
+    H4213 H4233 # XA2 Ultra
+)
+MSIM_DEVICE=0
+
+for device in "${MSIM_DEVICES[@]}"; do
+    if grep -q "Model: ${device}" /dev/block/bootdevice/by-name/LTALabel; then
+        MSIM_DEVICE=1
+        break
+    fi
+done
+
+if [[ "${MSIM_DEVICE}" -eq 1 ]]; then
+    setprop ro.vendor.radio.multisim.config dsds
+else
+    setprop ro.vendor.radio.multisim.config ss
+fi
