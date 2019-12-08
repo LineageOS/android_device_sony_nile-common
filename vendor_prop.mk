078c27e8a194491f466be1604c111d0f0aa1a4cf
LuK1337 <priv.luk@gmail.com>
2019-12-08 23:33:31 +0100

    nile-common: Move non-vendor props to system.prop
    Change-Id: I67fce77b9ccd05b1e754480c932b9eeda7749950

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -18,7 +18,6 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     af.fast_track_multiplier=1 \
     audio.deep_buffer.media=true \
-    audio.offload.min.duration.secs=30 \
     audio.offload.video=true \
     persist.vendor.audio.fluence.speaker=true \
     persist.vendor.audio.fluence.voicecall=true \
@@ -45,10 +44,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
-    bt.max.hfpclient.connections=1 \
-    qcom.bluetooth.soc=cherokee \
     ro.vendor.bt.bdaddr_path=/data/vendor/bluetooth/bluetooth_bdaddr \
-    vendor.bluetooth.soc=cherokee \
     vendor.qcom.bluetooth.soc=cherokee
 
 # CNE
@@ -72,8 +68,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
-    debug.gralloc.gfx_ubwc_disable=0 \
-    ro.qualcomm.cabl=0
+    debug.gralloc.gfx_ubwc_disable=0
 
 # DRM
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -90,8 +85,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.hw=1 \
     debug.sf.latch_unsignaled=1 \
     dev.pm.dyn_samplingrate=1 \
-    persist.demo.hdmirotationlock=false \
-    persist.hwc.enable_vds=1 \
     persist.sys.wfd.virtual=0 \
     ro.opengles.version=196610 \
     ro.sf.lcd_density=480 \
@@ -113,22 +106,16 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.vidc.enc.dcvs.extra-buff-count=2 \
     vendor.video.disable.ubwc=1
 
-# NFC
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.nfc.port=I2C
-
 # Perf
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=libqti-perfd-client.so
 
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
-    DEVICE_PROVISIONED=1 \
     persist.vendor.radio.apm_sim_not_pwdn=1 \
     persist.vendor.radio.custom_ecc=1 \
     persist.vendor.radio.rat_on=combine \
     persist.vendor.radio.sib16_support=1 \
-    ril.subscription.types=NV,RUIM \
     ro.telephony.default_network=9,1 \
     vendor.rild.libpath=/vendor/lib64/libril-qc-qmi-1.so
 
@@ -142,10 +129,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.sensors.pmd=true \
     ro.vendor.sensors.sta_detect=true
 
-# TimeService
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.timed.enable=true
-
 # USB
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config.extra=none \
@@ -155,8 +138,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.wifi.addr_path=/data/vendor/wifi/wlan_mac.bin \
     wifi.interface=wlan0
-
-# Wireless display
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.debug.wfd.enable=1 \
-    persist.sys.wfd.virtual=0
