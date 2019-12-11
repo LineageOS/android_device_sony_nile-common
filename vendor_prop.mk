cb7c3bf5be8d138d78a10e7ce9ee7356ea9e50ca
LuK1337 <priv.luk@gmail.com>
2019-12-11 14:44:25 +0100

    nile-common: Update WiFi MAC address path prop
    * macaddrsetup when compiled against API >=29
      uses that prop instead.
    
    Change-Id: Ieddeb097ebc870186d41fa6c538165e0237b1ab3

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -179,5 +179,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Wifi
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.wifi.addr_path=/data/vendor/wifi/wlan_mac.bin \
+    ro.vendor.wifi.addr_path=/data/vendor/wifi/wlan_mac.bin \
     wifi.interface=wlan0
