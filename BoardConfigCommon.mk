a6e212a349aec9b16050fe82d2c7fccca985dbf7
LuK1337 <priv.luk@gmail.com>
2020-05-10 11:30:24 +0200

    nile-common: Update blobs from 50.2.A.3.22
    Change-Id: I56bc48f2c751d45405897e482f2e298ac63a90da

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -43,6 +43,7 @@ TARGET_NO_BOOTLOADER := true
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 service_locator.enable=1 swiotlb=1 androidboot.configfs=true androidboot.usbcontroller=a800000.dwc3 loop.max_part=7
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
