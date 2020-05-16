200dfe2d47f6183233304102634a180f9db1a709
LuK1337 <priv.luk@gmail.com>
2020-05-16 20:28:31 +0200

    nile-common: Build android.hardware.camera.provider@2.4:64
    * Fixes: ims_media_jni: Error loading library lib-imsvt.so: dlopen failed: library "android.hardware.camera.provider@2.4.so" not found
    
    Change-Id: I938dd7135a53e9e0951c31e7f56082c0564cf924

--- nile.mk
+++ nile.mk
@@ -157,6 +157,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4:64 \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
     Snap \
