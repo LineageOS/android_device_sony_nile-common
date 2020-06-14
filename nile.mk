d53c5069afd232b1f7dbf9c7fada25c86cf6e80b
LuK1337 <priv.luk@gmail.com>
2020-06-14 19:18:43 +0200

    nile-common: Exclude overlay/packages/apps/Bluetooth from RRO
    Change-Id: Id94046b5d26202931943ef34c00c1d82f5ea84f6

--- nile.mk
+++ nile.mk
@@ -26,7 +26,9 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay-lineage/lineage-sdk
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay/packages/apps/Bluetooth \
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
 
 # Properties
 PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
