e14aaddd018dff7c9f81759d91f35817c45f93af
LuK1337 <priv.luk@gmail.com>
2020-09-27 18:12:13 +0200

    [DNM] nile-common: Bringup hacks
    Change-Id: I2b80a3beb931d11db4b8367cded738e935797002

--- nile.mk
+++ nile.mk
@@ -455,8 +455,8 @@ PRODUCT_PACKAGES += \
     libmediaextractorservice \
     libnl
 
-PRODUCT_BOOT_JARS += \
-    WfdCommon
+#PRODUCT_BOOT_JARS += \
+#    WfdCommon
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
