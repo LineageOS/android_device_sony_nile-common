91636bc362072350d938ebadff438e7a41324650
Vinoth Jayaram <c_vinoja@codeaurora.org>
2020-11-17 18:03:30 +0530

    nile-common: Updated Mapper & Allocator
    Updated Mapper & Allocator version.
    Enabled Gralloc4 target flag.
    
    Change-Id: I7103067f6c727a36c1a06439d37c7a15520f1166

--- nile.mk
+++ nile.mk
@@ -180,12 +180,12 @@ PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
 # Display
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.allocator@2.0-impl:64 \
-    android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-service \
-    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.graphics.mapper@3.0-impl-qti-display \
+    android.hardware.graphics.mapper@4.0-impl-qti-display \
     gralloc.sdm660 \
     hwcomposer.qcom \
+    vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.memtrack-service
 
 # DRM
