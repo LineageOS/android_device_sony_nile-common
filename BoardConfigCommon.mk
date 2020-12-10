c3bd76c5f342f5fca19b546b8c24670b08aa0094
LuK1337 <priv.luk@gmail.com>
2020-12-10 15:58:57 +0100

    nile-common: Update blobs from 53.1.A.3.85
    Change-Id: I7ca4068827b74574563a71374b73e607eb5347d3

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -199,7 +199,7 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2020-03-01
+VENDOR_SECURITY_PATCH := 2020-11-01
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
