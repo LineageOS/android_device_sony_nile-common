e67079131c271fbebb688271c1561d8ecd46d76a
Josh Wu <joshwu@google.com>
2022-01-24 22:56:14 -0800

    nile-common: Switch BtAudio to AIDL
    Test: m
    Bug: 203490261
    Change-Id: I4b8ac40d5ad410b89eb6a0573d740d86471ecfc7
    Merged-In: I4b8ac40d5ad410b89eb6a0573d740d86471ecfc7

--- nile.mk
+++ nile.mk
@@ -99,7 +99,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl:32 \
     android.hardware.audio.effect@6.0-impl:32 \
     android.hardware.audio.service \
-    android.hardware.bluetooth.audio@2.0-impl:32 \
+    android.hardware.bluetooth.audio-impl:32 \
     android.hardware.soundtrigger@2.2-impl:32 \
     audio.bluetooth.default \
     audio.primary.sdm660 \
