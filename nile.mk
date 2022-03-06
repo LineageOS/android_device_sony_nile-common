0fc8bd26fc273d3adf2a9e03633258f5a8ed6b58
Alexander Koskovich <akoskovich@pm.me>
2022-03-06 02:11:47 -0700

    nile-common: Update to Boot Control HAL v1.2
    Change-Id: Ib5d06c9b0c7e2ed000eb03a9bf18e4d78cc8740d

--- nile.mk
+++ nile.mk
@@ -133,9 +133,9 @@ PRODUCT_COPY_FILES += \
 
 # Boot control
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-impl-qti \
-    android.hardware.boot@1.1-impl-qti.recovery \
-    android.hardware.boot@1.1-service
+    android.hardware.boot@1.2-impl-qti \
+    android.hardware.boot@1.2-impl-qti.recovery \
+    android.hardware.boot@1.2-service
 
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
