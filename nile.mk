6d5d519ab5c72461d3c3bd413b64d9dc3810cbf2
Alexander Koskovich <akoskovich@pm.me>
2022-04-12 05:00:05 -0700

    nile-common: Pick up USB configurations from QCOM
    Change-Id: I83882d8992eb367211871130c163f4ca5689fcd4

--- nile.mk
+++ nile.mk
@@ -366,6 +366,9 @@ PRODUCT_PACKAGES_DEBUG += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.dual_role_usb
 
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/qcom/opensource/usb/etc
+
 # Vendor init
 $(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):libinit.sony_nile)
 
