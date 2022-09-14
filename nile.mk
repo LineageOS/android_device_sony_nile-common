d0ced360a00085f57f525d7789710d69620a71f1
Bruno Martins <bgcngm@gmail.com>
2022-09-14 14:51:15 +0100

    nile-common: Migrate to AIDL ClearKey DRM HAL
    Change-Id: Iad5cc1176978137c647069197b33f49fb4d98ab2

--- nile.mk
+++ nile.mk
@@ -198,7 +198,7 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl:64 \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.1.vendor \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm-service.clearkey
 
 # Fingerprint
 PRODUCT_PACKAGES += \
