0d5a9c21ca3cf1608f943c1cb46ebf9bab35bf81
Gabriel Biren <gbiren@google.com>
2023-03-27 22:33:41 +0000

    nile-common: Update makefile to use the AIDL Wifi Vendor HAL.
    Bug: 274964641
    Test: Pre-submit tests
    Change-Id: I4d9114377fe75b593bbfe6a59f03d8f5d607d6a1

--- nile.mk
+++ nile.mk
@@ -443,7 +443,7 @@ PRODUCT_COPY_FILES += \
 
 # WiFi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi-service \
     libwpa_client \
     hostapd \
     WifiOverlay \
