152c7c81bb8576691b24587683277012a73920da
Vivekachooz <vivekachooz@gmail.com>
2023-06-06 22:03:44 +0530

    nile-common: Build OSS camera hal
    Change-Id: I229b6dee32e8788cb4c613039f4133cd6e18105d

--- nile.mk
+++ nile.mk
@@ -144,7 +144,8 @@ $(call soong_config_set,QTI_GPT_UTILS,USE_BSG_FRAMEWORK,false)
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service
+    android.hardware.camera.provider@2.4-service \
+    camera.sdm660
 
 # Common init scripts
 PRODUCT_PACKAGES += \
