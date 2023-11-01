b20003858892e6164775fcfc108aaa189c7b5618
LuK1337 <priv.luk@gmail.com>
2023-11-01 18:11:13 +0100

    nile-common: Use libutils-v33 for pm-service
    Change-Id: Ia998fa72d8747de18010abe6491d2c374cb7aa0a

--- extract-files.sh
+++ extract-files.sh
@@ -63,6 +63,9 @@ function blob_fixup() {
         product/lib64/libdpmframework.so)
             sed -i "s/libhidltransport.so/libcutils-v29.so\x00\x00\x00/" "${2}"
             ;;
+        vendor/bin/pm-service)
+            grep -q libutils-v33.so "${2}" || "${PATCHELF}" --add-needed "libutils-v33.so" "${2}"
+            ;;
         vendor/bin/qns|vendor/lib/libSonyIMX300PdafLibrary.so)
             "${PATCHELF}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${2}"
             ;;
