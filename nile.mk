6ed26aaedf549ca03677cd1d372857903257c1bd
Bruno Martins <bgcngm@gmail.com>
2023-11-13 13:48:00 +0000

    nile-common: Move to QTI health AIDL service
    The health AIDL HAL service provides functionalities of charger,
    therefore system charger at /system/bin/charger is deprecated.
    
    Change-Id: I59c23e7974cea1174b0161f31a535fa3afa1e5c9

--- nile.mk
+++ nile.mk
@@ -226,8 +226,7 @@ PRODUCT_COPY_FILES += \
 
 # Healthd
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-service
+    android.hardware.health-service.qti
 
 # Input
 PRODUCT_COPY_FILES += \
