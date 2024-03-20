996b40ca4c04b7f1cd4404faa49cd0fefff60728
basamaryan <basam.aryan@gmail.com>
2024-03-20 01:16:12 -0400

    nile-common: Don't explicitly include updatable_apex.mk
    It is inherited by base_system.mk as of QPR2
    
    Change-Id: I020a05b233f61ea28d03199cbc76051e6e97e1dc

--- nile.mk
+++ nile.mk
@@ -14,9 +14,6 @@
 # limitations under the License.
 #
 
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/sony/nile-common/nile-common-vendor.mk)
 
