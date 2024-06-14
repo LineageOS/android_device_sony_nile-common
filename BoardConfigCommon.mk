5c9de952d3573fa7c136d4920864b43b959829c9
Michael Bestas <mikeioannina@gmail.com>
2024-06-15 00:46:19 +0300

    nile-common: Include lineage FCM
    Change-Id: Ib4408e941d920a3a38132ba1c8360994f5534c0b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -166,7 +166,8 @@ BOARD_HAVE_QCOM_FM := true
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := $(VENDOR_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(VENDOR_PATH)/compatibility_matrix.xml
 
