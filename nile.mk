d3e202d01bd846f1513ceda33a8618b51d4b0878
Michael Bestas <mkbestas@lineageos.org>
2024-09-26 01:39:46 +0300

    nile-common: Switch to AIDL USB HAL
    Change-Id: I70f6a4e57eef577d86987bfae44f381a4f107624

--- nile.mk
+++ nile.mk
@@ -365,7 +365,7 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service.dual_role_usb
+    android.hardware.usb-service.qti
 
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/usb/etc
