36de3d3908185cd1c82911a2a026d6c846e11635
LuK1337 <priv.luk@gmail.com>
2024-11-01 13:59:13 +0100

    nile-common: Ensure we build the vendor version of libhidlbase_shim
    Reference: I68bdc36f4bbeb19871706c90de517067e73bb741
    Change-Id: I858f7e4d4df02208ede82be32e42a1956640a02d

--- nile.mk
+++ nile.mk
@@ -435,7 +435,7 @@ PRODUCT_PACKAGES += \
 
 # VNDK
 PRODUCT_PACKAGES += \
-    libhidlbase_shim
+    libhidlbase_shim.vendor
 
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v33/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v33.so
