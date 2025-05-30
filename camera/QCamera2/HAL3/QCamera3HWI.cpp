19ffd1d2527bb420577c8eebe92b4fd8fc09f1d4
LuK1337 <priv.luk@gmail.com>
2025-05-30 12:04:07 +0200

    hax
    Change-Id: I93fb37980422acdd496f90b50a43d02b92fd1fbf

--- camera/QCamera2/HAL3/QCamera3HWI.cpp
+++ camera/QCamera2/HAL3/QCamera3HWI.cpp
@@ -597,9 +597,11 @@ QCamera3HardwareInterface::QCamera3HardwareInterface(uint32_t cameraId,
     if (gCamCapability[cameraId]->is_quadracfa_sensor) {
         m_bQuadraCfaSensor = true;
 
+#ifndef TARGET_NILE
         if (gCamCapability[cameraId]->is_quadracfa_insensor) {
             m_bInSensorQCFA = true;
         }
+#endif
 
         char prop[PROPERTY_VALUE_MAX];
         memset(prop, 0, sizeof(prop));
@@ -2889,7 +2891,9 @@ int QCamera3HardwareInterface::configureStreamsPerfLocked(
                 {
                     padding_info.width_padding = CAM_PAD_TO_512;
                     padding_info.height_padding = CAM_PAD_TO_512;
+#if !defined(TARGET_NILE) && !defined(TARGET_GANGES)
                     padding_info.usage = newStream->usage;
+#endif
                     mStreamConfigInfo[index].type[stream_index] = CAM_STREAM_TYPE_CALLBACK;
                     if ((m_bIs4KVideo && !isZsl) || (bSmallJpegSize && !isZsl)) {
                         mStreamConfigInfo[index].postprocess_mask[stream_index] =
@@ -11632,7 +11636,11 @@ int QCamera3HardwareInterface::initCapabilities(uint32_t cameraId)
     }
 
     if (gCamCapability[cameraId]->is_remosaic_lib_present ||
+#ifdef TARGET_NILE
+            false) {
+#else
             gCamCapability[cameraId]->is_quadracfa_insensor) {
+#endif
         gCamCapability[cameraId]->is_quadracfa_sensor = TRUE;
     }
 
