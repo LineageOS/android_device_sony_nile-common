c5dcd3d22717876f3b7d28c65dda5f2f1ef3a22e
LuK1337 <priv.luk@gmail.com>
2025-05-30 12:04:07 +0200

    nile-common: camera: Adapt for our device blobs
    Change-Id: I93fb37980422acdd496f90b50a43d02b92fd1fbf

--- camera/QCamera2/Android.mk
+++ camera/QCamera2/Android.mk
@@ -173,6 +173,12 @@ ifneq (,$(filter msm8996,$(TARGET_BOARD_PLATFORM)))
     LOCAL_CFLAGS += -DTARGET_MSM8996
 endif
 
+ifneq (,$(filter discovery pioneer voyager,$(TARGET_DEVICE)))
+    LOCAL_CFLAGS += -DTARGET_NILE
+else ifneq (,$(filter kirin mermaid,$(TARGET_DEVICE)))
+    LOCAL_CFLAGS += -DTARGET_GANGES
+endif
+
 LOCAL_CFLAGS += -DUSE_CAMERA_METABUFFER_UTILS
 
 #LOCAL_STATIC_LIBRARIES := libqcamera2_util
