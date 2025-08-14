631dc0096cdede2ab3c73a858b91fd4efbfb4050
LuK1337 <priv.luk@gmail.com>
2025-08-14 23:47:18 +0200

    nile-common: Migrate to AIDL LiveDisplay HAL
    Change-Id: I4e8be836e94d442b40c63a1ac7dade3c9d9c784c

--- nile.mk
+++ nile.mk
@@ -246,7 +246,7 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
+    vendor.lineage.livedisplay-service.sdm
 
 # Mac address setup
 PRODUCT_PACKAGES += \
