ded5d8faeb7f96799b4fa2ba8e714a65d7a4dd80
LuK1337 <priv.luk@gmail.com>
2025-09-27 15:36:43 +0200

    nile-common: Drop android.hardware.drm@1.0
    We are using AIDL Clearkey.
    
    Change-Id: Ife224b6a88f352225c2c1db9550265da15dd9f74

--- nile.mk
+++ nile.mk
@@ -193,8 +193,6 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl:64 \
-    android.hardware.drm@1.0-service \
     android.hardware.drm-service.clearkey
 
 # Fastboot
