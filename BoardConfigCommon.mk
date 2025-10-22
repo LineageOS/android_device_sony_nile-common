37e88b915f0ee4eb476c9ac123ab51e7229ae771
LuK1337 <priv.luk@gmail.com>
2025-10-22 13:03:04 +0200

    nile-common: Inherit libperfmgr sepolicy
    Change-Id: Icd98d95900c24c7b9b9ab492f810a14d71390fd9

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -194,6 +194,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := 2021-02-01
 
 # SELinux
+include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 
 BOARD_VENDOR_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy/vendor
